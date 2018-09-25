64aeefe1b73021c7edb0315204b56a92183b96ef
bajer83 <bajer@hot.pl>
2018-09-25 22:50:38 +0100

    Created a seperate thread for the serial reading funtion. Cleaned up code and make Text area reset itself so it doesn't consumen whole memory

--- program.py
+++ program.py
@@ -6,6 +6,7 @@ import serial
 import serial.tools.list_ports
 from datetime import datetime
 import threading
+import time
 
 
 class Application:
@@ -14,31 +15,30 @@ class Application:
         self.root = root
         self.root.title("DigiCourse Birds depths Excel helper")
 
-        parent = Frame(self.root)  # new frame inside which all widgets reside
-        self.combobox = Combobox(parent, values=self.read_available_com_ports())  # creating and initilising Combobox
+        # parent = Frame(self.root)  # new frame inside which all widgets reside
 
-        console_frame = Frame(self.root)
-        self.text_console = Text(console_frame)
-        self.text_console.pack()
+        Label(root, text="COM port:").grid(row=0, column=0, sticky=E)
 
-        Label(parent, text="Please select COM port that is connected to DigiCourse PC:").pack()
-        Label(parent, text="COM port:").pack(side="left")
-
-        self.combobox.pack(side="left")
+        self.combobox = Combobox(root, values=self.read_available_com_ports())  # creating and initilising Combobox
+        self.combobox.grid(row=0, column=1)
         self.combobox.current(0)  # sets the first index as a selected option
 
-        self.connect_button = Button(parent, text="Connect", command=self.button_pressed)
-        self.connect_button.pack(side="left")
+        self.connect_button = Button(root, text="Connect", command=self.open_serial_port_and_read_thread)
+        self.connect_button.grid(row=0, column=2, columnspan=1, sticky=NSEW)
 
-        parent.pack()
-        status_frame = Frame(self.root)
-        Label(status_frame, text="Status:").pack()
-        Label(status_frame, text="Found:").pack()
+        Separator(root, orient='horizontal').grid(row=2, column=0, columnspan=3, sticky=NSEW, pady=[10, 0])
+        Label(root, text="Status:").grid(row=3, column=0, sticky=W)
+        Label(root, text="Found:").grid(row=4, column=0, sticky=W)
 
-        status_frame.pack()
-        console_frame.pack()
+        self.text_console = Text(self.root, width=50, height=50)
+        self.text_console.grid(row=5, column=0, columnspan=3)
 
-        # text_console.insert('1.0', self.display_bird_information())
+        #scroll_bar = Scrollbar(self.root)
+        #self.text_console.configure(yscrollcommand=scroll_bar.set)
+        #scroll_bar.config(command=self.text_console.yview)
+       # scroll_bar.grid(row=5, column=4, sticky='NSW')
+
+    # Label(parent, text="Please select COM port that is connected to DigiCourse PC:")
 
     def toggle_connect_button_state(self):
         if self.connect_button.cget("text") == 'Connect':
@@ -50,9 +50,6 @@ class Application:
         # Creates a list with available COM ports using list comprehension
         return [comport.device for comport in serial.tools.list_ports.comports()]
 
-    def button_pressed(self):
-        self.open_serial_port_and_read_thread()
-
     # def read_file(self):
     #     data = '''Q12:04:380148505C020337C063293C102501C120390C1304130012BT01029803072658BT0202860
     # 1992851BT03028002051497BT04031302272774BT05029802732619BT06030402442774BT0703510
@@ -63,32 +60,36 @@ class Application:
     #     return data
 
     def open_serial_port_and_read(self):
-        with serial.Serial(self.combobox.get(), 9600, timeout=1) as ser:
+        with serial.Serial(self.combobox.get(), 9600, timeout=0) as ser:
             while True:
                 self.toggle_connect_button_state()
+
                 line = ser.readline()  # read a '\n' terminated line
                 data = str(line)
                 if '-' in data:
                     self.text_console.insert('1.0',
                                              'String incorrect. Possible cause - DigiBirds in sleep mode\n')
-                    return
+
                 if 'Q' in data:
                     data_to_save = self.parse_bird_data(data)  # extracts depths and returns a dictionary data type
                     self.write_to_file(data_to_save)
                     self.text_console.insert('1.0',
-                                             'Data saved at {} \n'.format(datetime.now().isoformat(timespec='seconds')) + str(
+                                             'Data saved at {} \n'.format(
+                                                 datetime.now().isoformat(timespec='seconds')) + str(
                                                  data_to_save))
                 else:
                     self.text_console.insert('1.0',
                                              'Found incorrect or no data: {} \n'.format(data))
-                    return
+                    self.text_console.delete("1.0", END)
+                    self.text_console.edit_reset()
+
 
     def open_serial_port_and_read_thread(self):
         """
         A threading version of the open serial and read method
         """
-        self.thread = threading.Thread(target=self.open_serial_port_and_read())
-        self.thread.start()
+        thread = threading.Thread(target=self.open_serial_port_and_read, daemon=TRUE)
+        thread.start()
 
     def write_to_file(self, data_to_save):
         with open(os.path.join(os.path.abspath('.'), 'digiBirdsDepthsLog.txt'), 'w') as save_file:
@@ -117,5 +118,7 @@ class Application:
 
 if __name__ == '__main__':
     main_root = Tk()
+    main_root.resizable(width=False, height=False)
+    main_root.geometry('{}x{}'.format(430, 200))
     app = Application(main_root)
     main_root.mainloop()
